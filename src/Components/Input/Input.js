e55d75fe789d2cdc7eab3a7db3288c27a5859cf5
shanmukha <shanmukhaciv@gmail.com>
2024-03-18 16:36:56 +0530

    loginchanges

--- src/Components/Input/Input.js
+++ src/Components/Input/Input.js
@@ -1,13 +1,14 @@
 import React from 'react'
 
 const Input = (props) => {
-    const {lbl,type,errorMsg}=props;
+    const {lbl,type,errorMsg,value,name,fnChange}=props;
+  
   return (
     <div>
       
 <div className='row mb-2'>
 <div className='col-sm-5 text-end'><b>{lbl}</b></div>
-<div className='col-sm-3 '><input  type ={type } className='form-control'/></div>
+<div className='col-sm-3 '><input  type ={type } name={name} value={value} className='form-control' onChange={fnChange}/></div>
 <div className='col-sm-4 text-danger'>{errorMsg}</div>
 </div>
     </div>
--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,18 +1,42 @@
+'use client'
 import Input from '@/Components/Input/Input'
-import React from 'react'
+import React, { useState } from 'react'
 import configure from './configuration.json'
 const Login = () => {
+  const [inputArr,setInputArr] = useState( configure)
+  const [data,setData] = useState({})
+  const fnLogin =()=>{
+ 
+    const clonedInputArr = JSON.parse(JSON.stringify( inputArr))
+    var dataobj = {}
+    clonedInputArr.forEach((obj)=>{
+ dataobj[obj.name] = obj.value
+    })
+    console.log(dataobj)
+  }
+  const  fnChange =(eve)=>{
+
+ const {name,value} = eve.target
+  const clonedInputArr = JSON.parse(JSON.stringify( inputArr))
+   const inputControlObject = clonedInputArr .find((obj)=>{
+      return obj.name === name
+
+     })
+     inputControlObject.value = value;
+     setInputArr(clonedInputArr )
+  
+  }
   return <div className='mt-3' >
 
 {
-    configure.map((obj,ind)=>{
-        return <Input {...obj} />
+    inputArr.map((obj,ind)=>{
+        return <Input key={`input_${ind}`} {...obj} fnChange={fnChange} />
 
     })
 }
 
 <div className='row'>
-    <div className='offset-5 col-sm-7'><button className='btn btn-primary'>Login</button></div>
+    <div className='offset-5 col-sm-7'><button className='btn btn-primary' onClick={fnLogin}>Login</button></div>
 </div>
   </div>
 }
